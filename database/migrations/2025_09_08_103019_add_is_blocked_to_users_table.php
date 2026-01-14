e0d644d3e3e4be4d4dd5dd68f1b7ec8e58586d01
smt197 <105449441+smt197@users.noreply.github.com>
2026-01-14 12:52:47 +0000

    Update 2025_09_08_103019_add_is_blocked_to_users_table.php

--- database/migrations/2025_09_08_103019_add_is_blocked_to_users_table.php
+++ database/migrations/2025_09_08_103019_add_is_blocked_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_blocked')->default(false)->after('confirmed');
+            $table->boolean('is_blocked')->default(false);
         });
     }
 
