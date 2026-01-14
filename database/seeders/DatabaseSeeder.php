212dbf8ad68c3c36fba29b576ab0371a9fa18de3
smt197 <105449441+smt197@users.noreply.github.com>
2026-01-14 12:41:11 +0000

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,19 +14,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // 1. Seeders de base (Rôles, Permissions, Pays, Traductions)
-        $this->call([
-            TranslationLoaderSeeder::class, // Si vous avez ce seeder
-            RoleSeeder::class,
-            PermissionSeeder::class,
-            PermissionRoleTableSeeder::class,
-            CountrySeeder::class,
-            CategorySeeder::class, // AVANT MenuSeeder
-            UserSeeder::class,
-            MenuSeeder::class,
-            ChatSeeder::class,
-            MaintenanceModeSeeder::class,
-            ModuleManagerSeeder::class,]);
-        $this->command->info('DatabaseSeeder a terminé avec succès.');
+        if (app()->environment('local')) {
+            $this->call([
+                TranslationLoaderSeeder::class, // Si vous avez ce seeder
+                RoleSeeder::class,
+                PermissionSeeder::class,
+                PermissionRoleTableSeeder::class,
+                CountrySeeder::class,
+                CategorySeeder::class, // AVANT MenuSeeder
+                UserSeeder::class,
+                MenuSeeder::class,
+                ChatSeeder::class,
+                MaintenanceModeSeeder::class,
+                ModuleManagerSeeder::class,
+            ]);
+            $this->command->info('DatabaseSeeder a terminé avec succès.');
+        }
+
+        if (app()->environment('production')) {
+            $this->call([
+                TranslationLoaderSeeder::class, // Si vous avez ce seeder
+                RoleSeeder::class,
+                PermissionSeeder::class,
+                PermissionRoleTableSeeder::class,
+                CountrySeeder::class,
+                CategorySeeder::class, // AVANT MenuSeeder
+                UserSeeder::class,
+                MenuSeeder::class,
+                ChatSeeder::class,
+                MaintenanceModeSeeder::class,
+                ModuleManagerSeeder::class,
+            ]);
+            $this->command->info('DatabaseSeeder a terminé avec succès.');
+        }
     }
 }
